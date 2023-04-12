b9e509e73a61d2af2b0337b5cf8329f1fbb633aa
Eduard <eduardkarapetyan1997@mail.ru>
2023-04-12 13:08:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,11 +9,25 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    _, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
-    alpha = 0.01
-    effect = (pval < alpha)
+  
+    alpha=0.01
     
-    return effect # Ваш ответ, True или False
+    p_value=proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1] / 2
+    
+    if (p_value < alpha) and (x_success/x_cnt < y_success/y_cnt):
+      return True
+    else: 
+      return False
+    
+# def solution(x_success: int, 
+#              x_cnt: int, 
+#              y_success: int, 
+#              y_cnt: int) -> bool:
+#     # Измените код этой функции
+#     # Это будет вашим решением
+#     # Не меняйте название функции и её аргументы
+#     _, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+#     alpha = 0.01
+#     effect = (pval < alpha)
+    
+#     return effect # Ваш ответ, True или False
