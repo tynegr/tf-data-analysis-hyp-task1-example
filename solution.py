5007a4108bf79bf979db2293dd36a21b11397e1e
Eduard <eduardkarapetyan1997@mail.ru>
2023-04-12 13:13:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import math
+from scipy import stats
 from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 298754188 # Ваш chat ID, не меняйте название переменной
