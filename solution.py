c609c3418b66f140e59af6337f67fd7d04937c58
tynegr <122130536+tynegr@users.noreply.github.com>
2023-04-13 00:07:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,16 +4,18 @@ import numpy as np
 
 chat_id = 462449141 # Ваш chat ID, не меняйте название переменной
 
+from statsmodels.stats.proportion import proportions_ztest
+
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    a = 0.04
-    mean1 = x_success/ x_cnt
-    mean2 = y_success/ y_cnt
-    answer = True
-    if mean2-mean1 < a:
-      answer = True
-    else:
-      answer = False
-    return answer
+    
+    alpha = 0.04
+    
+    counts = np.array([x_success, y_success])
+    nobs = np.array([x_cnt, y_cnt])
+    
+    _, p_value = proportions_ztest(counts, nobs, alternative = 'smaller')
+
+    return p_value < alpha # Ваш ответ, True или False
